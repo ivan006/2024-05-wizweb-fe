15cea948baa5b680e376ec061658483539aeb576
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-17 08:47:21 +0200

    1
    1
    
    1
    
    1

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,46 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: 'src/index.js', // Replace with the path to your entry file
+      entry: 'src/index.js',
       name: 'QuickLists',
       fileName: (format) => `quicklists.${format}.js`,
     },
     rollupOptions: {
-      // Make sure to externalize dependencies that you don't want to bundle
-      external: ['vue', 'vuetify'],
+      external: [
+        'vue',
+        'vuetify',
+        'moment',
+        'moment-timezone',
+        '@quasar/quasar-ui-qcalendar',
+        'quasar',
+        'vue3-google-map',
+      ],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
         globals: {
           vue: 'Vue',
           vuetify: 'Vuetify',
+          moment: 'moment',
+          'moment-timezone': 'momentTimezone',
+          quasar: 'Quasar',
         },
       },
+      plugins: [
+        commonjs(), // Keep CommonJS plugin
+      ],
+    },
+  },
+  optimizeDeps: {
+    include: ['moment', 'moment-timezone'], // Pre-bundle these dependencies
+  },
+  resolve: {
+    alias: {
+      moment: 'moment/moment.js', // Alias for moment
+      'moment-timezone': 'moment-timezone/builds/moment-timezone-with-data.js', // Alias for moment-timezone
     },
   },
 });
