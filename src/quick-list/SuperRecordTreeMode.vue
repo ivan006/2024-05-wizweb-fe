5c686420810263cc655e79a1804de6778d564fde
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-01-13 15:56:23 +0200

    1

--- src/quick-list/SuperRecordTreeMode.vue
+++ src/quick-list/SuperRecordTreeMode.vue
@@ -1,37 +1,51 @@
 <template>
   <div>
-    x
+    <q-tree
+        :nodes="treeData"
+        node-key="id"
+        :default-expand-all="true"
+        @lazy-load="handleLazyLoad"
+        @toggle="handleToggle"
+    >
+      <template v-slot:default="props">
+        <div>
+          <div v-if="props.node.type === 'record'">
+            <strong>{{ props.node.label }}</strong>
+          </div>
+
+          <div v-else-if="props.node.type === 'attr'">
+            {{ props.node.label }}: {{ props.node.value }}
+          </div>
+
+          <div v-else-if="props.node.type === 'relation'">
+            <em>{{ props.node.label }}</em>
+          </div>
+        </div>
+      </template>
+    </q-tree>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "SuperRecordTreeMode",
-  components: {
-  },
+  components: {},
   props: {
     configsCollection: {
       type: Object,
-      default: {},
+      default: () => ({}),
     },
     allowedTabs: {
       type: Array,
-      default() {
-        return [];
-      },
+      default: () => [],
     },
     templateOverview: {
       type: Object,
-      default() {
-        return {};
-      },
+      default: () => ({}),
     },
     templateForm: {
       type: Object,
-      default() {
-        return {};
-      },
+      default: () => ({}),
     },
     model: {
       type: [Object, Function],
@@ -47,10 +61,84 @@ export default {
     },
     relationships: {
       type: Array,
-      default() {
-        return [];
-      },
+      default: () => [],
     },
+  },
+  data() {
+    return {
+      treeData: [],
+      loading: false,
+    };
+  },
+  methods: {
+    async fetchData() {
+      this.loading = true;
+      try {
+        const response = await this.model.FetchById(this.id, this.relationships);
+        this.treeData = this.prepareTreeData(response.response.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    prepareTreeData(record) {
+      const attrs = Object.keys(record)
+          .filter(key => typeof record[key] !== "object")
+          .map(key => ({
+            type: "attr",
+            label: key,
+            value: record[key],
+          }));
+
+      const childGroups = this.relationships.map(relation => ({
+        type: "relation",
+        label: relation.label || relation.name,
+        lazy: true,
+        children: [],
+        model: relation.relatedModel,
+        parentId: this.id,
+      }));
+
+      return [
+        {
+          type: "record",
+          label: this.model.entity,
+          children: [...attrs, ...childGroups],
+        },
+      ];
+    },
+
+    handleLazyLoad(node, done) {
+      if (node.type !== "relation" || !node.lazy) {
+        done();
+        return;
+      }
+
+      const relationModel = node.model;
+      const parentId = node.parentId;
+
+      relationModel.Fetch({filters: {parent_id: parentId}})
+          .then(response => {
+            node.children = response.response.data.data.map(record => ({
+              type: "record",
+              label: relationModel.entity,
+              children: this.prepareTreeData(record)[0].children,
+            }));
+          })
+          .catch(error => {
+            console.error("Error fetching child data:", error);
+          })
+          .finally(() => done());
+    },
+
+    handleToggle(node) {
+      console.log("Toggled node:", node);
+    },
+  },
+  mounted() {
+    this.fetchData();
   },
 };
 </script>
