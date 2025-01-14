fa63a8d6c25030649e7b20a696ca628578dd0c9a
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-01-14 18:42:42 +0200

    1

--- src/quick-list/SuperRecordTreeMode.vue
+++ src/quick-list/SuperRecordTreeMode.vue
@@ -1,35 +1,64 @@
 <template>
   <div>
     <q-tree
-        :nodes="treeData"
-        node-key="id"
-        :default-expand-all="true"
+        :nodes="computedTreeStructure"
+        node-key="label"
+        default-expand-all
         @lazy-load="handleLazyLoad"
-        @toggle="handleToggle"
     >
       <template v-slot:default="props">
         <div>
-          <div v-if="props.node.type === 'record'">
-            <strong>{{ props.node.label }}</strong>
-          </div>
+          <template v-if="props.node.type === 'record'">
+            <div class="custom-record">
+              <strong>{{ props.node.label }}</strong>
+              <q-btn-group>
+                <q-btn flat round icon="visibility" @click="viewNode(props.node)" />
+                <q-btn flat round icon="edit" @click="editNode(props.node)" />
+                <q-btn flat round icon="delete" color="negative" @click="deleteNode(props.node)" />
+              </q-btn-group>
+            </div>
+          </template>
 
-          <div v-else-if="props.node.type === 'attr'">
-            {{ props.node.label }}: {{ props.node.value }}
-          </div>
+          <template v-else-if="props.node.type === 'attr'">
+            <div class="custom-attr">
+              <span>{{ props.node.label }}: </span>
+              <span>{{ props.node.value }}</span>
+              <q-btn-group>
+                <q-btn flat round icon="visibility" @click="viewNode(props.node)" />
+                <q-btn flat round icon="edit" @click="editNode(props.node)" />
+                <q-btn flat round icon="delete" color="negative" @click="deleteNode(props.node)" />
+              </q-btn-group>
+            </div>
+          </template>
 
-          <div v-else-if="props.node.type === 'relation'">
-            <em>{{ props.node.label }}</em>
-          </div>
+          <template v-else-if="props.node.type === 'parent'">
+            <div class="custom-parent">
+              <span>Parent: </span>
+              <SuperRecordTreeMode
+                  :tree-structure="props.node.children"
+                  :data="props.node.data"
+                  :relationships="props.node.relations"
+                  :active="false"
+              />
+              <q-btn-group>
+                <q-btn flat round icon="visibility" @click="viewNode(props.node)" />
+                <q-btn flat round icon="edit" @click="editNode(props.node)" />
+                <q-btn flat round icon="delete" color="negative" @click="deleteNode(props.node)" />
+              </q-btn-group>
+            </div>
+          </template>
         </div>
       </template>
     </q-tree>
+    <pre>{{computedTreeStructure}}</pre>
   </div>
 </template>
 
 <script>
+import QuickListsHelpers from "./QuickListsHelpers";
+
 export default {
   name: "SuperRecordTreeMode",
-  components: {},
   props: {
     configsCollection: {
       type: Object,
@@ -63,19 +92,52 @@ export default {
       type: Array,
       default: () => [],
     },
+    data: {
+      type: Object,
+      default: null,
+    },
+    treeStructure: {
+      type: Array,
+      default: () => [],
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
   },
   data() {
     return {
-      treeData: [],
       loading: false,
+      computedTreeStructure: [],
     };
+  },
+  computed: {
+    headers() {
+      return QuickListsHelpers.SupaerTableHeaders(this.model, [], this.canEdit, this.displayMapField);
+    },
+    superOptions() {
+      return {
+        headers: this.headers,
+        modelFields: QuickListsHelpers.computedAttrs(this.model, []),
+        displayMapField: this.displayMapField,
+        model: this.model,
+        canEdit: this.canEdit,
+      };
+    },
+    canEdit() {
+      return true;
+    },
   },
   methods: {
     async fetchData() {
+      if (!this.active) return; // Skip fetching if passive
+
       this.loading = true;
       try {
         const response = await this.model.FetchById(this.id, this.relationships);
-        this.treeData = this.prepareTreeData(response.response.data.data);
+        const fetchedData = response.response.data.data;
+        this.computedTreeStructure = this.computeTreeStructure(fetchedData, this.relationships);
+        this.$emit("update:data", fetchedData);
       } catch (error) {
         console.error("Error fetching data:", error);
       } finally {
@@ -83,65 +145,86 @@ export default {
       }
     },
 
-    prepareTreeData(record) {
-      const attrs = Object.keys(record)
-          .filter(key => typeof record[key] !== "object")
-          .map(key => ({
+    computeTreeStructure(data, relationships) {
+      const attrs = this.headers
+          .filter(header => !header.usageType.startsWith("relChildren"))
+          .map(header => ({
             type: "attr",
-            label: key,
-            value: record[key],
+            label: header.label,
+            value: data[header.field],
           }));
 
-      const childGroups = this.relationships.map(relation => ({
-        type: "relation",
-        label: relation.label || relation.name,
-        lazy: true,
-        children: [],
-        model: relation.relatedModel,
-        parentId: this.id,
-      }));
+      const parentRelations = relationships
+          .filter(relation => relation.type === "parent")
+          .map(relation => ({
+            label: relation.label || relation.name,
+            type: "parent",
+            children: data[relation.name]?.map(childData => this.computeTreeStructure(childData, relation.children || [])) || [],
+            data: data[relation.name],
+            relations: relation.children || [],
+          }))[0] || null;
 
-      return [
+      const childGroups = relationships
+          .filter(relation => relation.type === "child")
+          .map(relation => ({
+            label: relation.label || relation.name,
+            type: "child",
+            children: data[relation.name]?.map(childData => this.computeTreeStructure(childData, relation.children || [])) || [],
+            data: data[relation.name],
+            relations: relation.children || [],
+          }));
+
+      const treeNodes = [
         {
+          label: this.model.entity, // Root record label
           type: "record",
-          label: this.model.entity,
-          children: [...attrs, ...childGroups],
+          children: [...attrs, ...(parentRelations ? [parentRelations] : []), ...childGroups],
         },
       ];
+
+      return treeNodes;
     },
 
     handleLazyLoad(node, done) {
-      if (node.type !== "relation" || !node.lazy) {
+      if (!node.children || node.children.length === 0) {
         done();
         return;
       }
-
-      const relationModel = node.model;
-      const parentId = node.parentId;
-
-      relationModel.Fetch({filters: {parent_id: parentId}})
-          .then(response => {
-            node.children = response.response.data.data.map(record => ({
-              type: "record",
-              label: relationModel.entity,
-              children: this.prepareTreeData(record)[0].children,
-            }));
-          })
-          .catch(error => {
-            console.error("Error fetching child data:", error);
-          })
-          .finally(() => done());
+      done();
     },
 
-    handleToggle(node) {
-      console.log("Toggled node:", node);
+    viewNode(node) {
+      console.log("View node:", node);
+      // Add logic for viewing node details
+    },
+
+    editNode(node) {
+      console.log("Edit node:", node);
+      // Add logic for editing node
+    },
+
+    deleteNode(node) {
+      console.log("Delete node:", node);
+      // Add logic for deleting node
     },
   },
   mounted() {
-    this.fetchData();
+    if (this.active) {
+      this.fetchData();
+    } else {
+      this.computedTreeStructure = this.treeStructure;
+    }
   },
 };
 </script>
 
 <style lang="scss">
+.custom-record,
+.custom-attr,
+.custom-parent {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.5rem;
+}
 </style>
